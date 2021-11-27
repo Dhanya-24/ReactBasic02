4a0ea02a49516560387ff4a59e915e6f15588bb8
Dhanya-24 <dhanyaanu888@gmail.com>
2021-11-27 14:51:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import TaskView from "./components/06-CustomHooks/Task/Index";
+import Forms from "./components/07-Forms/Index";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Forms />;
 }
 
 export default App;
